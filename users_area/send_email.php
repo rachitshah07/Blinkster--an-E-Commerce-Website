79e4329acd01d361ac56ca4d59adca74bf3a0f12
rachitshah07 <72906451+rachitshah07@users.noreply.github.com>
2023-05-06 18:35:16 +0530

    Update send_email.php

--- users_area/send_email.php
+++ users_area/send_email.php
@@ -4,7 +4,7 @@ function smtp_mailer($to, $subject, $msg, $username)
     require_once 'config.php';
     require 'vendor/autoload.php';
     $email = new \SendGrid\Mail\Mail();
-    $email->setFrom("rachitshah0709@gmail.com", "Blinkster Admin");
+    $email->setFrom("your_registered_sendgrid_mail@gmail.com", "Blinkster Admin");
     $email->setSubject($subject);
     $email->addTo($to, $username);
     $email->addContent($msg);
